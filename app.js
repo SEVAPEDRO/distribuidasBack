6f4a2066a7d00d4b105ac78f44c4cc86ea0b8a05
Pedro Seva <pedrosevaseva@gmail.com>
2022-11-17 22:07:54 -0300

    change to resetPass

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
-app.disable('etag');
+//app.disable('etag');
 
 // error handler
 app.use(function (err, req, res, next) {
